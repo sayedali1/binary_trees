f80e2838bd4a0b8cad551e8f8a3ed30fe86da53b
sayedali1 <said.ali99859@gmail.com>
2023-08-04 02:30:05 +0300

    task11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,7 +10,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	int Lheight = 0;
 	int Rheight = 0;
 
-	if(!tree)
+	if (!tree)
 		return (0);
 	Lheight = binary_tree_size(tree->left);
 	Rheight = binary_tree_size(tree->right);
