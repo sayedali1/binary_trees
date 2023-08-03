021f1f4035311e000f8d0704b005df2b8a63fb05
sayedali1 <said.ali99859@gmail.com>
2023-08-04 02:52:32 +0300

    task17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,8 +10,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (node->parent->left != node)
-		return (node->parent->left);
-	else
+	if (node->parent->left == node)
 		return (node->parent->right);
+	return (node->parent->left);
 }
