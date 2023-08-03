8a28fdc31c1793d7918940750c81bf563832cb45
sayedali1 <said.ali99859@gmail.com>
2023-08-04 02:36:07 +0300

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,7 +10,29 @@ int binary_tree_balance(const binary_tree_t *tree)
 	int Lheight = 0;
 	int Rheigt = 0;
 
-	Lheight = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-	Rheigt = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	if (!tree)
+		return (0);
+	Lheight = tree->left ? binary_tree_height(tree->left) : -1;
+	Rheigt = tree->right ? binary_tree_height(tree->right) : -1;
 	return (Lheight - Rheigt);
 }
+
+/**
+ * binary_tree_height - function that measures
+ * the height of a binary tree
+ * @tree: pointer to thee root of the tree
+ * Return: the height
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	int Lheight = 0;
+	int Rheigt = 0;
+
+	if (!tree)
+		return (0);
+
+	Lheight = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	Rheigt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	return (Lheight > Rheigt ? Lheight : Rheigt);
+}
+
