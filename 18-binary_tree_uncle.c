0b7c2a1f7dc2c996d93c5bdb21817d96493a4837
sayedali1 <said.ali99859@gmail.com>
2023-08-04 02:55:49 +0300

    task18

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -23,8 +23,7 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (node->parent->left->n != node->n)
-		return (node->parent->left);
-	else
+	if (node->parent->left == node)
 		return (node->parent->right);
+	return (node->parent->left);
 }
