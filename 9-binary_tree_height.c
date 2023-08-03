90ee257278ec4f75e10ee912ac0515324403106c
sayedali1 <said.ali99859@gmail.com>
2023-08-04 02:23:20 +0300

    task9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,6 +11,9 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	int Lheight = 0;
 	int Rheigt = 0;
 
+	if (!tree)
+		return (0);
+
 	Lheight = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 	Rheigt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 	return (Lheight > Rheigt ? Lheight : Rheigt);
